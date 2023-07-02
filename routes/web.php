addc31df26f13d3b0fc3bc71ca62bf288d077ef6
Aqli Hamdan <aqli.hammdan46@gmail.com>
2023-07-02 23:48:51 +0700

    Add authentication
    - Add login page, register page with laravel ui :+1:
    - Fix some bugs
    
    To do :
    - Create DashboardNav to make clean code

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\FinishedController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\RawController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('/dashboard');
+    return view('/auth/login');
 });
 
 // Route::get('/finished', function(){
@@ -37,3 +38,7 @@ Route::resource('finished', FinishedController::class);
 
 Route::resource('raw', RawController::class);
 
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
