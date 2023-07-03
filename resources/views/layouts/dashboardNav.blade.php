2b565d29f514e6aa925243fbb7b2522fb282f98f
Aqli Hamdan <aqli.hammdan46@gmail.com>
2023-07-03 15:55:32 +0700

    Fix some error
    Adjust home and masterdata page :+1:

---
+++ resources/views/layouts/dashboardNav.blade.php
@@ -0,0 +1,134 @@
+@section('dashboardNav')
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="{{ url('/') }}" class="brand-link">
+      <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <span class="brand-text font-weight-light">Warehouse</span>
+    </a>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+      <!-- Sidebar Menu -->
+      <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          <!-- Add icons to the links using the .nav-icon class
+             with font-awesome or any other icon font library -->
+
+             <li class="nav-item">
+                <a href="{{ url('home') }}" class="nav-link">
+                  <i class="nav-icon fas fa-solid fa-box"></i>
+                  <p>
+                    Dashboard
+                  </p>
+                </a>
+              </li>
+
+             <li class="nav-item">
+                <a href="{{ url('masterdata') }}" class="nav-link">
+                  <i class="nav-icon fas fa-solid fa-box"></i>
+                  <p>
+                    Master Data
+                  </p>
+                </a>
+              </li>
+  
+              <li class="nav-item">
+                <a href="{{ url('finished') }}" class="nav-link">
+                  <i class="nav-icon fas fa-solid fa-box"></i>
+                  <p>
+                    Barang Jadi
+                    <i class="right fas fa-angle-left"></i>
+                  </p>
+                </a>
+                <ul class="nav nav-treeview">
+                  <li class="nav-item">
+                    <a href="" class="nav-link">
+                      <i class="far fa-circle nav-icon"></i>
+                      <p>In Coming</p>
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a href="../../index2.html" class="nav-link">
+                      <i class="far fa-circle nav-icon"></i>
+                      <p>Out Going</p>
+                    </a>
+                  </li>
+                </ul>
+              </li>
+  
+              <li class="nav-item">
+                <a href="{{ url('') }}" class="nav-link">
+                  <i class="nav-icon fas fa-boxes"></i>
+                  <p>
+                    Barang Setengah Jadi
+                    <i class="right fas fa-angle-left"></i>
+                  </p>
+                </a>
+                <ul class="nav nav-treeview">
+                  <li class="nav-item">
+                    <a href="" class="nav-link">
+                      <i class="far fa-circle nav-icon"></i>
+                      <p>In Coming</p>
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a href="../../index2.html" class="nav-link">
+                      <i class="far fa-circle nav-icon"></i>
+                      <p>Out Going</p>
+                    </a>
+                  </li>
+                </ul>
+              </li>
+              <li class="nav-item">
+                <a href="{{ url('child') }}" class="nav-link">
+                  <i class="nav-icon fas fa-solid fa-box"></i>
+                  <p>
+                    Child Part & Komponen
+                    <i class="right fas fa-angle-left"></i>
+                  </p>
+                </a>
+                <ul class="nav nav-treeview">
+                  <li class="nav-item">
+                    <a href="" class="nav-link">
+                      <i class="far fa-circle nav-icon"></i>
+                      <p>In Coming</p>
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a href="../../index2.html" class="nav-link">
+                      <i class="far fa-circle nav-icon"></i>
+                      <p>Out Going</p>
+                    </a>
+                  </li>
+                </ul>
+              </li>
+  
+              <li class="nav-item">
+                <a href="{{ url('raw') }}" class="nav-link">
+                  <i class="nav-icon fas fa-solid fa-box"></i>
+                  <p>
+                    Raw Material
+                    <i class="right fas fa-angle-left"></i>
+                  </p>
+                </a>
+                <ul class="nav nav-treeview">
+                  <li class="nav-item">
+                    <a href="" class="nav-link">
+                      <i class="far fa-circle nav-icon"></i>
+                      <p>In Coming</p>
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a href="../../index2.html" class="nav-link">
+                      <i class="far fa-circle nav-icon"></i>
+                      <p>Out Going</p>
+                    </a>
+                  </li>
+                </ul>
+              </li>
+      </nav>
+      <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+  </aside>
+@endsection
