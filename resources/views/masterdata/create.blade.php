63e43c6f13408e9b25600aab60145942063fb8e1
Aldi Dharmawan <aldidharmawan700@gmail.com>
2023-07-29 10:41:31 +0700

    update create form masterdata
    update create form masterdata

--- resources/views/masterdata/create.blade.php
+++ resources/views/masterdata/create.blade.php
@@ -1,213 +1,21 @@
-@extends('layout.template')
+@extends('layouts.app')
+@extends('layouts.dashboardNav')
 
-@section('konten')
-    
-<body class="hold-transition sidebar-mini layout-fixed">
-  <div class="wrapper">
-
-    <!-- Preloader -->
-    <div class="preloader flex-column justify-content-center align-items-center">
-      <img class="animation__shake" src="/dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
-    </div>
-
-    <!-- Navbar -->
-    <nav class="main-header navbar navbar-expand navbar-white navbar-light">
-      <!-- Left navbar links -->
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
-        </li>
-        <li class="nav-item d-none d-sm-inline-block">
-          <a href="{{ url('/') }}" class="nav-link">Home</a>
-        </li>
-        <li class="nav-item d-none d-sm-inline-block">
-          <a href="#" class="nav-link">Contact</a>
-        </li>
-      </ul>
-
-      <!-- Right navbar links -->
-      <ul class="navbar-nav ml-auto">
-        <!-- Navbar Search -->
-        <li class="nav-item">
-          <a class="nav-link" data-widget="navbar-search" href="#" role="button">
-            <i class="fas fa-search"></i>
-          </a>
-          <div class="navbar-search-block">
-            <form class="form-inline">
-              <div class="input-group input-group-sm">
-                <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
-                <div class="input-group-append">
-                  <button class="btn btn-navbar" type="submit">
-                    <i class="fas fa-search"></i>
-                  </button>
-
-                </div>
-              </div>
-            </form>
-          </div>
-        </li>
-
-        <!-- Messages Dropdown Menu -->
-        <li class="nav-item">
-          <a class="nav-link" data-widget="fullscreen" href="#" role="button">
-            <i class="fas fa-expand-arrows-alt"></i>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" data-widget="control-sidebar" data-controlsidebar-slide="true" href="#" role="button">
-            <i class="fas fa-th-large"></i>
-          </a>
-        </li>
-      </ul>
-    </nav>
-    <!-- /.navbar -->
-
-    <!-- Main Sidebar Container -->
-    <aside class="main-sidebar sidebar-dark-primary elevation-4">
-      <!-- Brand Logo -->
-      <a href="{{ url('/') }}" class="brand-link">
-        <img src="/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-light">Warehouse</span>
-      </a>
-
-      <!-- Sidebar -->
-      <div class="sidebar">
-        <!-- Sidebar Menu -->
-        <nav class="mt-2">
-          <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-            <!-- Add icons to the links using the .nav-icon class
-               with font-awesome or any other icon font library -->
-            <li class="nav-item">
-              <a href="#" class="nav-link">
-                <i class="nav-icon fas fa-tachometer-alt"></i>
-                <p>
-                  Dashboard
-                </p>
-              </a>
-            </li>
-
-            <li class="nav-item">
-              <a href="{{ url('masterdata') }}" class="nav-link active">
-                <i class="nav-icon fas fa-solid fa-box"></i>
-                <p>
-                  Master Data
-                </p>
-              </a>
-            </li>
-
-            <li class="nav-item">
-              <a href="{{ url('finished') }}" class="nav-link">
-                <i class="nav-icon fas fa-solid fa-box"></i>
-                <p>
-                  Barang Jadi
-                  <i class="right fas fa-angle-left"></i>
-                </p>
-              </a>
-              <ul class="nav nav-treeview">
-                <li class="nav-item">
-                  <a href="" class="nav-link">
-                    <i class="far fa-circle nav-icon"></i>
-                    <p>In Coming</p>
-                  </a>
-                </li>
-                <li class="nav-item">
-                  <a href="../../index2.html" class="nav-link">
-                    <i class="far fa-circle nav-icon"></i>
-                    <p>Out Going</p>
-                  </a>
-                </li>
-              </ul>
-            </li>
-
-            <li class="nav-item">
-              <a href="{{ url('') }}" class="nav-link">
-                <i class="nav-icon fas fa-boxes"></i>
-                <p>
-                  Barang Setengah Jadi
-                  <i class="right fas fa-angle-left"></i>
-                </p>
-              </a>
-              <ul class="nav nav-treeview">
-                <li class="nav-item">
-                  <a href="" class="nav-link">
-                    <i class="far fa-circle nav-icon"></i>
-                    <p>In Coming</p>
-                  </a>
-                </li>
-                <li class="nav-item">
-                  <a href="../../index2.html" class="nav-link">
-                    <i class="far fa-circle nav-icon"></i>
-                    <p>Out Going</p>
-                  </a>
-                </li>
-              </ul>
-            </li>
-            <li class="nav-item">
-              <a href="{{ url('child') }}" class="nav-link">
-                <i class="nav-icon fas fa-solid fa-box"></i>
-                <p>
-                  Child Part & Komponen
-                  <i class="right fas fa-angle-left"></i>
-                </p>
-              </a>
-              <ul class="nav nav-treeview">
-                <li class="nav-item">
-                  <a href="" class="nav-link">
-                    <i class="far fa-circle nav-icon"></i>
-                    <p>In Coming</p>
-                  </a>
-                </li>
-                <li class="nav-item">
-                  <a href="../../index2.html" class="nav-link">
-                    <i class="far fa-circle nav-icon"></i>
-                    <p>Out Going</p>
-                  </a>
-                </li>
-              </ul>
-            </li>
-
-            <li class="nav-item">
-              <a href="{{ url('raw') }}" class="nav-link">
-                <i class="nav-icon fas fa-solid fa-box"></i>
-                <p>
-                  Raw Material
-                  <i class="right fas fa-angle-left"></i>
-                </p>
-              </a>
-              <ul class="nav nav-treeview">
-                <li class="nav-item">
-                  <a href="" class="nav-link">
-                    <i class="far fa-circle nav-icon"></i>
-                    <p>In Coming</p>
-                  </a>
-                </li>
-                <li class="nav-item">
-                  <a href="../../index2.html" class="nav-link">
-                    <i class="far fa-circle nav-icon"></i>
-                    <p>Out Going</p>
-                  </a>
-                </li>
-              </ul>
-            </li>
-        </nav>
-        <!-- /.sidebar-menu -->
-      </div>
-      <!-- /.sidebar -->
-    </aside>
-
+@section('content')
+      
     <div class="content-wrapper">
     <!-- Content Header (Page header) -->
     <section class="content-header">
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1>Input Master Data</h1>
+            <h1>Input Barang</h1>
           </div>
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item"><a href="{{ url('/') }}">Home</a></li>
               <li class="breadcrumb-item active">Master Data</li>
-              <li class="breadcrumb-item active">Input Barang </li>
+              <li class="breadcrumb-item active">Input Barang</li>
             </ol>
           </div>
         </div>
@@ -236,41 +44,45 @@
             <!-- general form elements -->
             <div class="card card-primary">
               <div class="card-header">
-                <h3 class="card-title">Input Barang</h3>
+                <h3 class="card-title">Transaksi Masuk</h3>
               </div>
               <!-- /.card-header -->
               <!-- form start -->
               <form action="{{ url('masterdata')}}" method="post">
                 @csrf
                 <div class="card-body container">
+                    <div class="form-group">
+                        <label for="kategori">Pilih Kategori</label>
+                        <select type="text" class="form-control" id="text" name="kategori">
+                            <option value="">- Pilih -</option>
+                            <option value="">- 1 -</option>
+                            <option value="">- 2 -</option>
+                            <option value="">- 3 -</option>
+
+                            {{-- @foreach ($kategori as $item)
+                            <option value="{{$item ->id}}">{{$item->nama}}</option>  
+                            @endforeach --}}
+                        </select>
+                      </div>
+
                   <div class="form-group">
-                    <label for="nama-barang">Nama Barang</label>
-                    <input type="text" class="form-control" id="text" name="nama_barang" placeholder="Nama Barang" >
+                    <label for="exampleInputPassword1">Nama Barang</label>
+                    <input type="text" class="form-control" id="nama_barang" name="nama_barang" placeholder="Nama Barang" >
                   </div>
+
                   <div class="form-group">
-                    <label for="kode-barang">Kode Barang</label>
-                    <input type="text" class="form-control" id="text" name="kode_barang" placeholder="Kode Barang" >
+                    <label for="exampleInputPassword1">Nama Supplier</label>
+                    <input type="text" class="form-control" id="nama_supplier" name="nama_supplier" placeholder="Nama Supplier" >
                   </div>
+
                   <div class="form-group">
-                    <label for="nama-customer">Nama Customer</label>
-                    <input type="text" class="form-control" id="text" name="nama_customer" placeholder="Nama Customer" >
+                    <label for="exampleInputPassword1">Kode Barang</label>
+                    <input type="text" class="form-control" name="kode_barang" id="kode_barang" >
                   </div>
+
                   <div class="form-group">
-                    <label for="kategori">Kategori</label>
-                    <br>
-                    <select class="form-select form-select-lg mb-3" name="kategori" aria-label=".form-select-lg example">
-                      <option selected>Pilih Kategori</option>
-                      <option value="1">Barang Jadi</option>
-                      <option value="2">Barang Setengah Jadi</option>
-                    </select>
-                  </div>
-                  <div class="form-group">
-                    <label for="exampleInputPassword1">Tanggal</label>
-                    <input type="date" class="form-control" name="tanggal" id="date" >
-                  </div>
-                  <div class="form-group">
-                    <label for="exampleInputPassword1">Jumlah</label>
-                    <input type="number" class="form-control" name="jumlah" id="date" >
+                    <label for="exampleInputPassword1">Jenis</label>
+                    <input type="text" class="form-control" name="jenis" id="jenis" >
                   </div>
         
                 </div>
@@ -286,7 +98,6 @@
             </div>
         <!-- ./wrapper -->
 
-     
         
 </body>
 
